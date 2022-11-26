a1a4b4cdd21240702e38ec7d384fcef21fd729ac
Igor Suvorov <hi@isuvorov.com>
2022-11-26 10:48:14 +0300

    fix: quickfix

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('*', (req, res) => {
-  res.send('Hello World!, API_KEY:' + process.env.API_KEY )
+  res.send('Hello World! API_KEY:' + process.env.API_KEY )
 })
 
 app.listen(port, () => {
